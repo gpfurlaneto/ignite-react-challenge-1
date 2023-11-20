016bf95088e76ed0cbf2dcbf783ab763f476e1d2
Guilherme Furlaneto <guilhermefurlaneto@Guilhermes-MacBook-Pro.local>
2023-11-20 18:14:18 -0300

    Fix images empty list

--- src/components/TaskList/EmptyList.tsx
+++ src/components/TaskList/EmptyList.tsx
@@ -1,11 +1,13 @@
 import styles from './EmptyList.module.css'
 
 export function EmptyList() {
-	return <div className={styles.emptyList}>
-		<img src='/clipboard.svg'/>
+	return (
+		<div className={styles.emptyList}>
+			<img src='/clipboard.svg' alt="Doc - Empty task list"/>
 			<p>
 				<strong>Você ainda não tem tarefas cadastradas</strong>
 				Crie tarefas e organize seus itens a fazer
 			</p>
-	</div>
+		</div>
+	)
 }
